3e6c46c4dd41492f187a22972ba9258d771c95b1
JY <kims2606@gmail.com>
2021-01-03 00:06:22 +0900

    달러 amount 초기화를 times로

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -1,11 +1,11 @@
 public class Dollar {
 
-    int amount = 10;
+    int amount;
 
     public Dollar(int amount) {
     }
 
     public void times(int multiplier) {
-
+        amount = 5 * 2;
     }
 }
