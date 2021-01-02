30e811dd9e4c8d0af41485ac5528fd813b5b64ae
JY <kims2606@gmail.com>
2021-01-03 00:43:49 +0900

    Dollar equals() 구현.

--- src/test/DollarTest.java
+++ src/test/DollarTest.java
@@ -16,5 +16,6 @@ public class DollarTest {
     @Test
     public void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(6));
     }
 }
